d122cedee87d2ef7efab260ab481c853eaa2eb69
Usieli23 <usieli12@gmail.com>
2024-11-23 23:20:54 -0300

    Arquivos do site 1

---
+++ Index JS-1.js
@@ -0,0 +1,5 @@
+// Adiciona funcionalidade ao formulário de contato
+document.getElementById('formContato').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita recarregar a página
+    alert('Formulário enviado com sucesso!');
+});
